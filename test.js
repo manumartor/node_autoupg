59a6b0b39e552537249bffd0e02c2ff643c38e07
manu.martor <manu.martor@gmail.com>
2016-01-07 10:16:11 -0500

    1st import code

---
+++ test.js
@@ -0,0 +1,5 @@
+/* test.js v.1.0.0 */
+
+var autoUpg = require('./index.js');
+console.log(autoUpg);
+//autoUpg.start();
---
+++ test/basictest.js
@@ -0,0 +1,7 @@
+/* test/basictest.js */
+
+describe("Basic test", function () {
+  it("It's Jasmine working", function () {
+    expect(true).toBe(true);
+  });
+});
